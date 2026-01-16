a1340257a4815ad8828361456d49e4e0cd605950
mino <81564697+umeshio@users.noreply.github.com>
2026-01-16 11:36:15 +0900

    パス周り調整

--- astro.config.ts
+++ astro.config.ts
@@ -24,29 +24,7 @@ export default defineConfig({
     // @ts-ignore
     plugins: [visualizer(), tailwindcss()],
     build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) => {
-            let extType = assetInfo.name?.split('.').pop();
-            if (/ttf|otf|eot|woff|woff2/i.test(extType || '')) {
-              extType = 'assets/fonts';
-            } else if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType || '')) {
-              extType = 'astroAssets';
-            } else if (extType === 'css') {
-              // swiper等のjs経由でcssが複数バンドルされてしまう時
-              // @ts-ignore
-              // if (assetInfo.name.includes('index.css')) {
-              //   return `assets/css/style.css`;
-              // }
-              return `assets/css/[name].css`;
-            } else {
-              return `${extType}/[name][extname]`;
-            }
-            return `${extType}/[name][extname]`;
-          },
-          entryFileNames: 'assets/js/common.js',
-        },
-      },
+      rollupOptions: { output: { assetFileNames: 'assets/[name][extname]' } },
     },
   },
   build: {
